9eca043c88953af34e4e5d407bed7c6f297ea6d9
Bots.Business <git.sync@bots.business>
2019-10-30 19:02:22 +1000

    fix with git sync

--- commands/GitSync/_setup-git.js
+++ commands/GitSync/_setup-git.js
@@ -12,8 +12,11 @@ CMD*/
 if(completed_commands_count==0){ return }
 
 url = Libs.Webhooks.getUrlFor(
-   { command: "on-git-push", user_id: user.id }
+   { command: "/on-git-push", user_id: user.id }
 )
 
-Bot.sendMessage("If you need Git Sync please see: https://help.bots.business/git/automatic-importing-on-git-push");
+Bot.sendMessage(
+  "If you need Git Sync please see: " + 
+  "https://help.bots.business/git/automatic-importing-on-git-push"
+);
 Bot.sendMessage("Your Git payload url: `" + url + "`" );
