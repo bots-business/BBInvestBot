c74314184daaeae236fce916046ea4bb66fe189c
Bots.Business <git.sync@bots.business>
2019-11-12 17:40:13 +1000

    add note after CP setup

--- commands/Admin/_setupcoinpayments.js
+++ commands/Admin/_setupcoinpayments.js
@@ -38,3 +38,10 @@ var bb_key = getConfigValue("CPOptions", "BB_KEY")
 Libs.CoinPayments.setPrivateKey(private_key);
 Libs.CoinPayments.setPublicKey(public_key);
 Libs.CoinPayments.setBBApiKey(bb_key);
+
+Bot.sendMessageToChatWithId(
+  Base.getAdminTgId(),
+  "CoinPayments keys - installed" +
+  "\n\n⚠️ You need set IPN secret also." + 
+  "\nPlease see this [help](https://help.bots.business/libs/coinpayments#setup-set-ipn-secret)"
+)
