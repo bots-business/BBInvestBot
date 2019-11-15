15e4758141f8b564da4409162da7ea02e5f26b9c
Bots.Business <git.sync@bots.business>
2019-11-15 19:43:54 +1000

    change setup languages to AP data

--- commands/Admin/_setup-languages.js
+++ commands/Admin/_setup-languages.js
@@ -15,9 +15,13 @@ if(!can_run){ return }
 
 var languages = Base.getLanguages();
 
+var cfg = AdminPanel.getPanelValues("GeneralInformation");
+
+var fields = []
+
 for(var i in languages){
   cmdName = "lng-" + languages[i];
-  Bot.run({ command: cmdName, options: CONFIGS })
+  Bot.run({ command: cmdName, options: cfg })
 }
 
 Bot.sendMessage("Multi Languages - installed");
